c70b62a6b7d02adfae847c63e26db5693212361c
dmataruiz <dmataruiz96@gmail.com>
2026-09-25 16:45:20 +0200

    changes to report in qase

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -5,23 +5,15 @@ import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.DataProvider;
 
 @CucumberOptions(
-        // Ruta a la carpeta donde están tus archivos .feature
         features = "src/test/resources/features",
-
-        // Paquetes donde Cucumber buscará las Step Definitions y los Hooks
         glue = {"tests", "hooks"},
-
-        // Filtro por etiquetas/tags (p. ej. ejecutar solo @Smoke o @Regression)
         tags = "@Smoke or @Regression",
-
-        // Reportes de ejecución
         plugin = {
-                "pretty",                                                // Muestra la salida formateada en consola
-                "html:target/cucumber-reports/cucumber-pretty.html",     // Reporte HTML visual
-                "json:target/cucumber-reports/cucumber.json"            // Reporte JSON para herramientas CI/CD
+                "pretty",
+                "html:target/cucumber-reports/cucumber-pretty.html",
+                "json:target/cucumber-reports/cucumber.json",
+                "io.qase.cucumber7.QaseEventListener"
         },
-
-        // Limpia caracteres extraños en los logs de la consola
         monochrome = true
 )
 public class TestRunner extends AbstractTestNGCucumberTests {
@@ -31,7 +23,7 @@ public class TestRunner extends AbstractTestNGCucumberTests {
      * Si no quieres ejecución en paralelo, elimina este método.
      */
     @Override
-    @DataProvider(parallel = true)
+    @DataProvider(parallel = false)
     public Object[][] scenarios() {
         return super.scenarios();
     }
